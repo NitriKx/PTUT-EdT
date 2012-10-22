4bfa3c29298b2716be01ed2ebf676bd9b3d8c0e2
Remy Bienvenu <remy.Bienvenu@gmail.com>
2012-10-22 20:42:54 +0200

    ajout de l'adresse de l'EdT dans @string

--- gen/com/iut/ptut/R.java
+++ gen/com/iut/ptut/R.java
@@ -17,9 +17,10 @@ public final class R {
     public static final class id {
         public static final int button1=0x7f070002;
         public static final int button2=0x7f070003;
-        public static final int menu_settings=0x7f070004;
+        public static final int menu_settings=0x7f070005;
         public static final int textView1=0x7f070001;
         public static final int textView2=0x7f070000;
+        public static final int textView3=0x7f070004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -28,6 +29,7 @@ public final class R {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
+        public static final int adresse=0x7f040005;
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int made_by=0x7f040004;
