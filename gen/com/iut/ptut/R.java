3ee48800b144750134572b4171b46380b766597c
NitriKx <nitrikxisback@gmail.com>
2012-10-23 14:00:40 +0200

    Decolle le bouton de 20dp du bord de l'ecran

--- gen/com/iut/ptut/R.java
+++ gen/com/iut/ptut/R.java
@@ -10,34 +10,37 @@ package com.iut.ptut;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        public static final int margin_bouton=0x7f040000;
+    }
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int buttonAnnuler=0x7f070003;
-        public static final int buttonOK=0x7f070002;
-        public static final int linearLayout1=0x7f070000;
-        public static final int madebyLabel=0x7f070004;
-        public static final int menu_settings=0x7f070005;
-        public static final int textView1=0x7f070001;
+        public static final int buttonAnnuler=0x7f080003;
+        public static final int buttonOK=0x7f080002;
+        public static final int linearLayout1=0x7f080000;
+        public static final int madebyLabel=0x7f080004;
+        public static final int menu_settings=0x7f080005;
+        public static final int textView1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int adresse=0x7f040005;
-        public static final int appName=0x7f040006;
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int made_by=0x7f040004;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_main=0x7f040003;
+        public static final int adresse=0x7f050005;
+        public static final int appName=0x7f050006;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
+        public static final int made_by=0x7f050004;
+        public static final int menu_settings=0x7f050002;
+        public static final int title_activity_main=0x7f050003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
