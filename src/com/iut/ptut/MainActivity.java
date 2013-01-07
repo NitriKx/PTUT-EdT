db2fb6df13fb674e1dc9ad5ec6e9221b0cda5032
hugz <hugz.roman@gmail.com>
2013-01-07 17:21:58 +0100

    solution pour gitignore

--- src/com/iut/ptut/MainActivity.java
+++ src/com/iut/ptut/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends Activity {
 		// Méthode java (NE PAS UTILISER). Le XML est beaucoup plus simple
 		// menu.add("testMenu");
 		// menu.add("testMenu2");
-		int useless;
+		int useless =28;
 		return true;
 	}
 	
