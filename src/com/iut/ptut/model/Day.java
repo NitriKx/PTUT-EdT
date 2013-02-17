90182779630ce4fd280b6d8bb409f09719bd91bd
jeremie rouelle <jeremie.rouelle@gmail.com>
2013-02-17 16:51:31 +0100

    ajout classes modeles
    class day/lesson/timetable

---
+++ src/com/iut/ptut/model/Day.java
@@ -0,0 +1,75 @@
+import java.util.Date;
+import java.util.Vector;
+
+
+public class Day {
+
+	
+	
+	private int idDay;
+	private Date DateDebut;
+	private Date DateFin;
+	private Vector<Lesson> ListLesson;
+	
+	
+	
+	public int getIdDay() {
+		return this.idDay;
+	}
+	public void setIdDay(int pIdDay) {
+		this.idDay = pIdDay;
+	}
+	public Date getDateDebut() {
+		return this.DateDebut;
+	}
+	public void setpDateDebut(Date pDateDebut) {
+		this.DateDebut = pDateDebut;
+	}
+	public Date getDateFin() {
+		return DateFin;
+	}
+	public void setpDateFin(Date pDateFin) {
+		this.DateFin = pDateFin;
+	}
+	public Vector<Lesson> getListLesson() {
+		return this.ListLesson;
+	}
+	public void setListLesson(Vector<Lesson> listLesson) {
+		this.ListLesson = listLesson;
+	}
+	
+	//ajout d'un cours  dans une journée
+	public void addLesson(Lesson pLesson)
+	{
+		this.ListLesson.add(pLesson);
+	}
+	
+	//suppression d'un cours  dans une journée
+	public void removeLesson(Lesson pLesson)
+	{
+		this.ListLesson.remove(pLesson);
+	}
+	
+	//suppression de tous les cours d'une journée
+	public void removeAllLessons(Lesson pLesson)
+	{
+		this.ListLesson.removeAllElements();
+	}
+	
+	
+	
+	
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/com/iut/ptut/model/TimeTable.java
@@ -0,0 +1,58 @@
+import java.util.Date;
+import java.util.Vector;
+
+
+
+public class TimeTable {
+
+	
+	private int idTT;
+	private Date dateDebut;
+	private Date dateFin;
+	private Vector<Day> ListDay;
+	
+	public int getId() {
+		return idTT;
+	}
+	public void setId(int pId) {
+		this.idTT = pId;
+	}
+	public Date getDateDebut() {
+		return this.dateDebut;
+	}
+	public void setDateDebut(Date pDateDebut) {
+		this.dateDebut = pDateDebut;
+	}
+	public Date getDateFin() {
+		return this.dateFin;
+	}
+	public void setDateFin(Date pDateFin) {
+		this.dateFin = pDateFin;
+	}
+	public Vector<Day> getListJour() {
+		return this.ListDay;
+	}
+	public void setListJour(Vector<Day> pListJour) {
+		this.ListDay = pListJour;
+	}
+	
+	//ajout d'un jour pDay à la liste de jours ListDay
+	public void addDay(Day pDay){
+		this.ListDay.add(pDay);
+	}
+	
+	//suppression d'un jour pDay à la liste de jours ListDay
+	public void removeDay(Day pDay){
+		this.ListDay.remove(pDay);
+	}
+	
+	
+	//suppression de TOUS les jours pDay de la liste de jours ListDay
+	public void removeAllDays(){
+		this.ListDay.removeAllElements();
+	}
+	
+	
+	
+	
+}
